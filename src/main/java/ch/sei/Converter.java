d912dd77457fb3659b32369179f374abaf386c92
RBLU <reto.blunschi@youpers.com>
2018-06-06 10:09:26 +0200

    solution step 1

--- src/main/java/ch/sei/Converter.java
+++ src/main/java/ch/sei/Converter.java
@@ -1,8 +1,19 @@
 package ch.sei;
 
-public class Converter   {
-    public long convert(String from, String to, double value) {
+import java.util.HashMap;
 
-        return 10000;
+public class Converter   {
+    static HashMap<String, Double> units = new HashMap();
+
+    public Converter() {
+        units.put("m", 1.0);
+        units.put("km", 1000.0);
+        units.put("cm", 0.01);
+        units.put("y", 0.9);
+        units.put("mi", 1590.0);
+    }
+
+    public double convert(String from, String to, double value) {
+        return value * units.get(from) / units.get(to);
     }
 }
--- src/test/java/ch/sei/ConverterTest.java
+++ src/test/java/ch/sei/ConverterTest.java
@@ -7,17 +7,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class ConverterTest {
 
     @Test
-    void convertMtoCM() {
+    void convertMtoM() {
         Converter c = new Converter();
-        double result = c.convert("m", "cm", 100);
-        assertEquals(10000,result);
+        double result = c.convert("m", "m", 100);
+        assertEquals(100,result);
+    }
+
+    @Test
+    void convertMtoKM() {
+        Converter c = new Converter();
+        double result = c.convert("m", "km", 1);
+        assertEquals(0.001,result);
     }
 
     @Test
     void convertKMtoCM() {
         Converter c = new Converter();
-        double result = c.convert("km", "cm", 100);
-        assertEquals(10000000,result);
+        double result = c.convert("km", "cm", 1);
+        assertEquals(100000,result);
+    }
+
+    @Test
+    void convertMtoY() {
+        Converter c = new Converter();
+        double result = c.convert("m", "y", 1);
+        assertEquals(1.1111111111111112,result);
     }
 
 }
