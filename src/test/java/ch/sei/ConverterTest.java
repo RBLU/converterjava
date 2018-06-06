dfeb7e233baf39b9aa2ec54fcb543bb60268c95c
RBLU <reto.blunschi@youpers.com>
2018-06-06 08:58:32 +0200

    added first test

--- src/test/java/ch/sei/ConverterTest.java
+++ src/test/java/ch/sei/ConverterTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ConverterTest {
 
     @Test
-    void convert() {
+    void convertMtoCM() {
         Converter c = new Converter();
         long result = c.convert("m", "cm", 100);
         assertEquals(10000,result);
